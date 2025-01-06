797cce43abf8e5feae8b9d48e1a6c10269424317
subahan00 <subahanmulla007@gmail.com>
2025-01-06 22:29:52 +0530

    Delete backend directory

--- backend/models/News.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const newsSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('News', newsSchema);
